07de834bcaf28cfd3c002ff18df2a9cf1947a5d8
Ari Virgil-Paige <avpaige@gmail.com>
2019-08-09 22:38:27 -0700

    framework of projects page js

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -1,6 +1,18 @@
   $(document).ready(function(){
 
+ var titles = ["Project Manager", "Web Developer",  "Operations & Logistics Expertise", "Problem Solver"]
+ 
+ setTimeout(twoSeconds, 1000 * 5);
+ function twoSeconds() {
+ }
 
+
+ $(document).ready(function(){
+ for (var i =0 ; i< titles.length; i++){
+        twoSeconds( $(".titles").empty().text(i)) 
+ 
+    }
+ });
 
 
 
---
+++ assets/javascript/projects.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+
+var bWoke = {
+    name: "bWoke",
+    git: "https://github.com/Kalamath/bWoke",
+    live: "https://kalamath.github.io/bWoke/",
+    img: "./assets/images/bwoke.jpg",
+    project: "bWoke is a coalition of wed developers at the intersection of entertainment and philanthorpy. Utilzing the Google Location, Spotify and Charity Navigator APIs to connect users with searched charities in a energizing and immersive experince.",
+
+}
+
+var rap = {
+    name: "Rapper$ Delight",
+    git: "https://github.com/Avpaige/RapperDelight",
+    live: "https://avpaige.github.io/RapperDelight/",
+    img: "./assets/images/rapper.png",
+    project: "Leveraging the Giphy API, find gifs based on your favorite rappers or other search terms and dynamically populate the page as well get rating information.",
+
+}
+
+var order = {
+    name: "Order Rush",
+    git: "https://github.com/Avpaige/unit-4-game",
+    live: "https://avpaige.github.io/unit-4-game/",
+    img: "./assets/images/bwoke.jpg",
+    project: "Generating a random order and menu price total using JQuery this game allows the use to test their math skills based on based arthmetic and randomization.",
+
+}
+
+var cleo = {
+    name: "Beat Miss Cleo",
+    git: "https://github.com/Avpaige/Psychic-Game",
+    live: "https://avpaige.github.io/Psychic-Game/",
+    img: "./assets/images/bwoke.jpg",
+    project: "Test your sixth sense by seeing if you can beat Miss Cleo in this vanilla Javascript based game that uses randomization.",
+
+}
+
+var train = {
+    name: "Train Scheduler",
+    git: "https://github.com/Avpaige/TrainTime",
+    live: "https://avpaige.github.io/TrainTime/",
+    img: "./assets/images/bwoke.jpg",
+    project: "Never be late again with this train scheduler that leverages moment.js and jQuery to calculate a variety of times.",
+
+}
+
+var trivia = {
+    name: "Movie Trivia",
+    git: "https://github.com/Avpaige/TriviaGame",
+    live: "https://avpaige.github.io/TriviaGame/",
+    img: "./assets/images/bwoke.jpg",
+    project: "Test your movie knowledege with this jQuery powered game with time events to keep you on the clock.",
+
+}
+
+
+
+
+
+
+
+});
